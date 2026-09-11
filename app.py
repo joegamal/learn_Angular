7075c12e41942a55c753a85c13df2176a8c48cc3
joegamal <yusufmagamalth@gmail.com>
2026-09-11 18:32:42 +0300

    clean up

--- app.py
+++
@@ -1,8 +0,0 @@
-import falcon
-
-class HelloResource:
-    def on_get(self, req, resp):
-        resp.media = {"message": "Hello, World!"}
-
-app = falcon.App()
-app.add_route("/hello", HelloResource())
